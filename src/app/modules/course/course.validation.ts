eb65abd8cd2fc07005d99de194373aaca6d06f3b
easakibahmad <sakib.sgc.du@gmail.com>
2023-12-12 01:44:20 +0600

    update course validation schema created

--- src/app/modules/course/course.validation.ts
+++ src/app/modules/course/course.validation.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 
+// creating validation schema
 export const createTagValidationSchema = z.object({
   name: z.string({ required_error: "Tag name is required" }),
 });
@@ -30,4 +31,46 @@ export const createCourseValidationSchema = z.object({
   details: createDetailsValidationSchema,
 });
 
-export const courseValidationSchema = { createCourseValidationSchema };
+// updating validation schema
+export const updateTagValidationSchema = z.object({
+  name: z.string({ required_error: "Tag name is required" }).optional(),
+});
+
+export const updateDetailsValidationSchema = z.object({
+  level: z
+    .string()
+    .refine(
+      (value) => ["Beginner", "Intermediate", "Advanced"].includes(value),
+      {
+        message:
+          "Invalid course level. Must be one of: Beginner, Intermediate, Advanced",
+      }
+    ).optional(),
+  description: z.string({ required_error: "Course description is required" }).optional(),
+});
+
+export const updateCourseValidationSchema = z.object({
+  title: z.string({ required_error: "Course title is required" }).optional(),
+  instructor: z
+    .string({ required_error: "Instructor name is required" })
+    .optional(),
+  categoryId: z
+    .string({ required_error: "Category ID is required" })
+    .optional(),
+  price: z.number({ required_error: "Course price is required" }).optional(),
+  tags: z.array(updateTagValidationSchema).optional(),
+  startDate: z.string({ required_error: "Start date is required" }).optional(),
+  endDate: z.string({ required_error: "End date is required" }).optional(),
+  language: z
+    .string({ required_error: "Course language is required" })
+    .optional(),
+  provider: z
+    .string({ required_error: "Provider name is required" })
+    .optional(),
+  details: updateDetailsValidationSchema.optional(),
+});
+
+export const courseValidationSchema = {
+  createCourseValidationSchema,
+  updateCourseValidationSchema,
+};
