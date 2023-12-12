6442ba7069efbbcd61f30eb5c65a9060a4ff58ff
easakibahmad <sakib.sgc.du@gmail.com>
2023-12-13 03:48:44 +0600

    create course date validation modified

--- src/app/modules/course/course.validation.ts
+++ src/app/modules/course/course.validation.ts
@@ -1,6 +1,9 @@
 import { boolean, z } from "zod";
 
 // creating validation schema
+
+const dateFormat = (value: string) => /^\d{4}-\d{2}-\d{2}$/.test(value);
+
 export const createTagValidationSchema = z.object({
   name: z.string({ required_error: "Tag name is required" }),
 });
@@ -24,8 +27,20 @@ export const createCourseValidationSchema = z.object({
   categoryId: z.string({ required_error: "Category ID is required" }),
   price: z.number({ required_error: "Course price is required" }),
   tags: z.array(createTagValidationSchema),
-  startDate: z.string({ required_error: "Start date is required" }),
-  endDate: z.string({ required_error: "End date is required" }),
+  startDate: z
+    .string({
+      required_error: "Start date is required",
+    })
+    .refine((value) => dateFormat(value), {
+      message: "Invalid date format. Please use 'YYYY-MM-DD'.",
+    }),
+  endDate: z
+    .string({
+      required_error: "End date is required",
+    })
+    .refine((value) => dateFormat(value), {
+      message: "Invalid date format. Please use 'YYYY-MM-DD'.",
+    }),
   language: z.string({ required_error: "Course language is required" }),
   provider: z.string({ required_error: "Provider name is required" }),
   details: createDetailsValidationSchema,
@@ -62,8 +77,18 @@ export const updateCourseValidationSchema = z.object({
     .optional(),
   price: z.number({ required_error: "Course price is required" }).optional(),
   tags: z.array(updateTagValidationSchema).optional(),
-  startDate: z.string({ required_error: "Start date is required" }).optional(),
-  endDate: z.string({ required_error: "End date is required" }).optional(),
+  startDate: z
+    .string({ required_error: "Start date is required" })
+    .refine((value) => dateFormat(value), {
+      message: "Invalid date format. Please use 'YYYY-MM-DD'.",
+    })
+    .optional(),
+  endDate: z
+    .string({ required_error: "End date is required" })
+    .refine((value) => dateFormat(value), {
+      message: "Invalid date format. Please use 'YYYY-MM-DD'.",
+    })
+    .optional(),
   language: z
     .string({ required_error: "Course language is required" })
     .optional(),
