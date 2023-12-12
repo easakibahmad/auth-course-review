0ede0fa81d27982ba584f545e3cc77a483345e78
easakibahmad <sakib.sgc.du@gmail.com>
2023-12-12 16:11:41 +0600

    review api modified

--- src/app/modules/review/review.controller.ts
+++ src/app/modules/review/review.controller.ts
@@ -4,9 +4,25 @@ import SendResponse from "../../utils/SendResponse";
 import { courseModel } from "../course/course.model";
 import httpStatus from "http-status";
 import { reviewServices } from "./review.service";
+import { createReviewValidationSchema } from "./review.validation";
 
 const createReview = catchAsync(async (req, res) => {
-  const reviewData = req.body;
+    const reviewData = req.body;
+    
+    const updatedDataKeys = Object.keys(reviewData); // check updated data keys
+
+    const updateSchemaKeys = Object.keys(createReviewValidationSchema.shape); //check schema keys
+
+    const invalidKeys = updatedDataKeys.filter(
+      (key) => !updateSchemaKeys.includes(key)
+    ); //get invalid keys
+
+    if (invalidKeys.length > 0) {
+      throw new AppError(
+        httpStatus.BAD_REQUEST,
+        "Invalid keys found for review creating"
+      );
+    }
 
   const checkCourseExistOrNot = await courseModel.findOne({
     _id: reviewData.courseId,
@@ -18,11 +34,19 @@ const createReview = catchAsync(async (req, res) => {
 
   const result = await reviewServices.createReviewIntoDB(reviewData);
 
+  // response data
+  const resultForResponse = {
+    _id: result._id,
+    courseId: result.courseId,
+    rating: result.rating,
+    review: result.review,
+  };
+
   SendResponse(res, {
     success: true,
     statusCode: httpStatus.CREATED,
     message: "Review created successfully",
-    data: result,
+    data: resultForResponse,
   });
 });
 
